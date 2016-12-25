461a5ba9e04b52fd6b6af924314b9120361d639c
AlexanderSwed <swedalexander@gmail.com>
2016-12-25 16:53:30 +0200

    Frontend almost ready

---
+++ server/server.js
@@ -0,0 +1,61 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    Mov = require('../model/db/movies');
+
+const config = require('./config');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('build'))
+mongoose.connect(config.mongoose.uri);
+const db = mongoose.connection;
+
+//app.use(express.static('views'));
+
+app.get('/', (req,res) => {
+    res.send("Use /api/mov for the API");
+});
+
+app.get('/api/movies', (req, res) => {
+    Mov.getMovies( function(err, movies) {
+        if (err) throw new Error(err);
+        res.json(movies);
+    });
+});
+
+app.get('/api/movies/:_id', (req, res) => {
+    Mov.getMovieById(req.params._id, function(err, movie) {
+        if (err) { throw new Error(err); }
+        res.json(movie);
+    });
+});
+
+app.post('/api/movies', (req, res) => {
+    const movie = req.body;
+    Mov.addMovie( movie, function(err, movie) {
+        if (err) { throw new Error(err); }
+        res.json(movie);
+    });
+});
+
+app.put('/api/movies/:_id', (req, res) => {
+    const id = req.params._id,
+        movie = req.body;
+    Mov.upMovie( id, movie, {}, function(err, movie) {
+        if (err) { throw new Error(err); }
+        res.json(movie);
+    });
+});
+
+app.delete('/api/movies/:_id', (req, res) => {
+    const id = req.params._id;
+    Mov.delMovie( id, function(err, movie) {
+        if (err) { throw new Error(err); }
+        res.json(movie);
+    });
+});
+
+app.listen(config.port, function(){
+    console.log(`Express server listening on port ${config.port}`);
+});
